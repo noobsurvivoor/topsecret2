a15a9890f1561df27ea36045156c8ed5ce3d16f2
ErickJVP <erickjgdev@gmail.com>
2024-02-11 20:28:28 -0300

    add rust project

---
+++ target/debug/deps/topsecret2-0a6ce95a26c75b10.d
@@ -0,0 +1,5 @@
+/Users/atheno/Documents/topsecret2/target/debug/deps/libtopsecret2-0a6ce95a26c75b10.rmeta: src/main.rs
+
+/Users/atheno/Documents/topsecret2/target/debug/deps/topsecret2-0a6ce95a26c75b10.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/topsecret2-cdcac316900204cc.d
@@ -0,0 +1,5 @@
+/Users/atheno/Documents/topsecret2/target/debug/deps/libtopsecret2-cdcac316900204cc.rmeta: src/main.rs
+
+/Users/atheno/Documents/topsecret2/target/debug/deps/topsecret2-cdcac316900204cc.d: src/main.rs
+
+src/main.rs:
